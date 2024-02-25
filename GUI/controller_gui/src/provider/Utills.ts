e0905a6fd2a10993f6dc6f3b8a485397b960eb7b
kirchner <kirchner@visionconnect.de>
2024-02-25 19:29:33 +0100

    updated fetchJson

--- GUI/controller_gui/src/provider/Utills.ts
+++ GUI/controller_gui/src/provider/Utills.ts
@@ -1,10 +1,12 @@
 const SERVER_BASE_URL = ""; // ADD URL of your Server here, or use localhost
-const LOCALHOST_BASE_URL = "http://127.0.0.1:5000";
 
-export const fetchJson = async (url: string, options = {}, useLocalhost: boolean): Promise<any> => {
-    let request_url = SERVER_BASE_URL + url;
-    if(useLocalhost) request_url = LOCALHOST_BASE_URL + url
-    const response = await fetch(request_url, options);
+const LOCALHOST_BASE_URL = "http://localhost";
+const API_PORT = ":5000";
+
+export const fetchJson = async (url: string, options = {}, useLocalhost: boolean = false): Promise<any> => {
+    const baseUrl = useLocalhost ? LOCALHOST_BASE_URL + API_PORT : SERVER_BASE_URL;
+    const requestUrl = `${baseUrl}${url}`;
+
+    const response = await fetch(requestUrl, options);
     return response.json();
 };
-
